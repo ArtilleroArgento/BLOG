c2286f6667de72beb0d11a60973d82bd6710b5dc
Emmanuel <emmanuelramosoficial@gmail.com>
2024-03-15 15:39:44 -0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm, LoginForm, PostForm
+from .models import Post
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'create_post.html', {'form': form})
